df0bc84577d6073493372cd00c9c193803a9e0fc
Eric Tyler <83565207+tylereric24@users.noreply.github.com>
2023-06-04 18:51:07 -0700

    Create data.py

---
+++ data.py
@@ -0,0 +1,303 @@
+#Game Data
+data = [
+    {
+        'name': 'Tom Brady',
+        'touchdowns': 28,
+        'team': 'New England Patriots',
+        'year': '2002'
+    },
+    {
+        'name': 'Drew Brees',
+        'touchdowns': 34,
+        'team': 'New Orleans Saints',
+        'year': '2009'
+    },
+    {
+        'name': 'Peyton Manning',
+        'touchdowns': 37,
+        'team': 'Denver Broncos',
+        'year': '2012'
+    },
+    {
+        'name': 'Brett Favre',
+        'touchdowns': 33,
+        'team': 'Minnesota Vikings',
+        'year': '2009'
+    },
+    {
+        'name': 'Aaron Rodgers',
+        'touchdowns': 22,
+        'team': 'Green Bay Packers',
+        'year': '2002'
+    },
+    {
+        'name': 'Troy Aikman',
+        'touchdowns': 23,
+        'team': 'Dallas Cowboys',
+        'year': '1992'
+    },
+    {
+        'name': 'Dan Marino',
+        'touchdowns': 44,
+        'team': 'Miami Dolphins',
+        'year': '1986'
+    },
+    {
+        'name': 'Vinny Testaverde',
+        'touchdowns': 25,
+        'team': 'New York Jets',
+        'year': '2000'
+    },
+    {
+        'name': 'Philip Rivers',
+        'touchdowns': 29,
+        'team': 'San Diego Chargers',
+        'year': '2015'
+    },
+    {
+        'name': 'Eli Manning',
+        'touchdowns': 26,
+        'team': 'New York Giants',
+        'year': '2016'
+    },
+    {
+        'name': 'Fran Tarkenton',
+        'touchdowns': 29,
+        'team': 'New York Giants',
+        'year': '1967'
+    },
+    {
+        'name': 'Matthew Stafford',
+        'touchdowns': 41,
+        'team': 'Detroit Lions',
+        'year': '2011'
+    },
+    {
+        'name': 'Russell Wilson',
+        'touchdowns': 34,
+        'team': 'Seattle Seahawks',
+        'year': '2017'
+    },
+    {
+        'name': 'Joe Montana',
+        'touchdowns': 17,
+        'team': 'San Francisco 49ers',
+        'year': '1982'
+    },
+    {
+        'name': 'George Blanda',
+        'touchdowns': 36,
+        'team': 'Houston',
+        'year': '1961'
+    },
+    {
+        'name': 'Peyton Manning',
+        'touchdowns': 25,
+        'team': 'Indianapolis Colts',
+        'year': '2005'
+    },
+    {
+        'name': 'Colt McCoy',
+        'touchdowns': 14,
+        'team': 'Cleveland Browns',
+        'year': '2011'
+    },
+    {
+        'name': 'Mac Jones',
+        'touchdowns': 22,
+        'team': 'New England Patriots',
+        'year': '2021'
+    },
+    {
+        'name': 'Joe Burrow',
+        'touchdowns': 13,
+        'team': 'Cincinnati Bengals',
+        'year': '2020'
+    },
+    {
+        'name': 'Kyler Murray',
+        'touchdowns': 26,
+        'team': 'Arizona Cardinals',
+        'year': '2020'
+    },
+    {
+        'name': 'Baker Mayfield',
+        'touchdowns': 26,
+        'team': 'Cleveland Browns',
+        'year': '2020'
+    },
+    {
+        'name': 'Donovan McNabb',
+        'touchdowns': 18,
+        'team': 'Philadelphia Eagles',
+        'year': '2006'
+    },
+    {
+        'name': 'Matt Ryan',
+        'touchdowns': 35,
+        'team': 'Atlanta Falcons',
+        'year': '2018'
+    },
+    {
+        'name': 'Matt Ryan',
+        'touchdowns': 26,
+        'team': 'Atlanta Falcons',
+        'year': '2020'
+    },
+    {
+        'name': 'Ryan Tannehill',
+        'touchdowns': 33,
+        'team': 'Tennessee Titans',
+        'year': '2020'
+    },
+    {
+        'name': 'Drew Bledsoe',
+        'touchdowns': 24,
+        'team': 'Buffalo Bills',
+        'year': '2002'
+    },
+    {
+        'name': 'Russell Wilson',
+        'touchdowns': 16,
+        'team': 'Denver Broncos',
+        'year': '2022'
+    },
+    {
+        'name': "Russell Wilson",
+        'touchdowns': 26,
+        'team': 'Seattle Seahawks',
+        'year': '2013'
+    },
+    {
+        'name': 'Andy Dalton',
+        'touchdowns': 27,
+        'team': 'Cincinnati Bengals',
+        'year': '2012'
+    },
+    {
+        'name': 'Tony Romo',
+        'touchdowns': 31,
+        'team': 'Dallas Cowboys',
+        'year': '2013'
+    },
+    {
+        'name': 'Peyton Manning',
+        'touchdowns': 55,
+        'team': 'Denver Broncos',
+        'year': '2013'
+    },
+    {
+        'name': 'Ben Roethlisberger',
+        'touchdowns': 26,
+        'team': 'Pittsburgh Steelers',
+        'year': '2009'
+    },
+    {
+        'name': 'Ben Roethlisberger',
+        'touchdowns': 34,
+        'team': 'Pittsburgh Steelers',
+        'year': '2018'
+    },
+    {
+        'name': 'Cam Newton',
+        'touchdowns': 35,
+        'team': 'Carolina Panthers',
+        'year': '2015'
+    },
+    {
+        'name': 'Kurt Warner',
+        'touchdowns': 36,
+        'team': 'St. Louis Rams',
+        'year': '2001'
+    },
+    {
+        'name': 'Cam Newton',
+        'touchdowns': 24,
+        'team': 'Carolina Panthers',
+        'year': '2018'
+    },
+    {
+        'name': 'Derek Carr',
+        'touchdowns': 32,
+        'team': 'Oakland Raiders',
+        'year': '2016'
+    },
+    {
+        'name': 'Tom Brady',
+        'touchdowns': 50,
+        'team': 'New England Patriots',
+        'year': '2007'
+    },
+    {
+        'name': 'Kirk Cousins',
+        'touchdowns': 26,
+        'team': 'Minnesota Vikings',
+        'year': '2019'
+    },
+    {
+        'name': 'Kirk Cousins',
+        'touchdowns': 35,
+        'team': 'Minnesota Vikings',
+        'year': '2020'
+    },
+    {
+        'name': 'Aaron Rodgers',
+        'touchdowns': 25,
+        'team': 'Green Bay Packers',
+        'year': '2018'
+    },
+    {
+        'name': 'Jake Plummer',
+        'touchdowns': 24,
+        'team': 'Arizona Cardinals',
+        'year': '1999'
+    },
+    {
+        'name': 'Patrick Mahomes',
+        'touchdowns': 26,
+        'team': 'Kansas City Chiefs',
+        'year': '2019'
+    },
+    {
+        'name': 'Tom Brady',
+        'touchdowns': 43,
+        'team': 'Tampa Bay Buccaneers',
+        'year': '2021'
+    },
+    {
+        'name': 'Patrick Mahomes',
+        'touchdowns': 41,
+        'team': 'Kansas City Chiefs',
+        'year': '2022'
+    },
+    {
+        'name': 'Jim Everett',
+        'touchdowns': 29,
+        'team': 'St Louis Rams',
+        'year': '1989'
+    },
+    {
+        'name': 'Joe Montana',
+        'touchdowns': 31,
+        'team': 'San Francisco 49ers',
+        'year': '1987'
+    },
+    {
+        'name': 'Jay Cutler',
+        'touchdowns': 23,
+        'team': 'Chicago Bears',
+        'year': '2010'
+    },
+    {
+        'name': 'Joe Flacco',
+        'touchdowns': 22,
+        'team': 'Baltimore Ravens',
+        'year': '2012'
+    },
+    {
+        'name': 'Carson Palmer',
+        'touchdowns': 26,
+        'team': 'Arizona Cardinals',
+        'year': '2016'
+    }
+]
